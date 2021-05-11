aa2db749589e6b2ceab0d7b343d624c951866394
PapAvetisyan <avetisyanpap18@gmail.com>
2021-05-12 00:08:07 +0400

    changed spiral

--- src/day9/Task9.java
+++ src/day9/Task9.java
@@ -3,16 +3,15 @@ package day9;
 import java.util.Scanner;
 
 public class Task9 {
-    //kisat
+    //spiral
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("n=");
         int n = input.nextInt();
         int[][] matrix = new int[n][n];
-        int l = 0, z = n , s = 0;
-        while (z > 0) {
+        int l = 0, z = n , s = 0,k = 0;
+        while (z >= 2) {
             int i = l, j;
-
             for (j = s; j < n - l; ++j) {
                 matrix[i][j] = 1;
             }
@@ -24,15 +23,16 @@ public class Task9 {
             for (j = n - l; j > l - s; --j) {
                 matrix[n - l][j] = 1;
             }
-            for (i = n - l; i > l ; --i) {
-                matrix[i][j] = 1;
+            for (i = n - l - k; i > l ; --i) {
+                matrix[i][j + k] = 1;
             }
+            ++k;
             ++l;
             z -= 4;
         }
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
-                System.out.print(matrix[i][j] + " ");
+                System.out.print(matrix[i][j] + "  ");
             }
             System.out.println();
         }
